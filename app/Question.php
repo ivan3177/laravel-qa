f630422654317b74eabd4d929e2ac9d8678493b7
Ivan Sokolovskii <ivan3177@gmail.com>
2019-01-13 12:35:00 +0300

    Add votes, answers and views counters to question item Add debug bar to dev enviroment

--- app/Question.php
+++ app/Question.php
@@ -18,4 +18,25 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
     }
+
+    public function getUrlAttribute()
+    {
+        return route('questions.show', $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return 'accepted';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
+    }
 }
