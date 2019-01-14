242721cf54dd8d9201a41acd77e43b0775cb072b
Ivan Sokolovskii <ivan3177@gmail.com>
2019-01-14 14:56:34 +0300

    Add vote controls to questions and answers

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,14 +36,18 @@
 
                                 <div class="ml-auto">
                                     @can('update', $question)
-                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info"><i class="fa fa-edit icons"> Edit</i></a>
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">
+                                        <i class="fa fa-edit fa-2x"> Edit</i>
+                                    </a>
                                     @endcan
 
                                     @can('delete', $question)
                                     <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
                                         @method('DELETE')
                                         @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')"><i class="fa fa-remove icons"> Delete</i></button>
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">
+                                            <i class="fa fa-remove fa-2x"> Delete</i>
+                                        </button>
                                     </form>
                                     @endcan
                                 </div>
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -6,26 +6,46 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1>{{ $question->title }}</h1>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="d-flex align-items-center">
+                            <h1>{{ $question->title }}</h1>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card-body">
-                    {!! $question->body_html !!}
+                    <hr>
 
-                    <div class="float-right">
-                        <span class="text-muted">Asked {{ $question->created_date }}</span>
-                        <div class="media mt-2">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}">
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a class="vote-up" title="This question is useful">
+                                <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <div class="media-body mt-1">
-                                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                            <span class="votes-count">5</span>
+                            <a class="vote-down off" title=" This question is not useful">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark as favorite question(click again to undo)" class="favorite mt-2 favorited">
+                                <i class="fas fa-star fa-2x"></i>
+
+                                <span class="favorites-count mt-1">123</span>
+                            </a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+
+                            <div class="float-right">
+                                <span class="text-muted">Asked {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -33,6 +53,7 @@
             </div>
         </div>
     </div>
+
     <div class="row mt-4">
         <div class="col-md-12">
             <div class="card">
@@ -43,6 +64,18 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                     <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a class="vote-up" title="This answer is useful">
+                                <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">5</span>
+                            <a class="vote-down off" title=" This answer is not useful">
+                                <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Click to mark as best answer" class="vote-accepted mt-2">
+                                <i class="fas fa-check fa-2x"></i>
+                            </a>
+                        </div>
                         <div class="media-body">
                             {!! $answer->body !!}
 
