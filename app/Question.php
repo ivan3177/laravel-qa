17f66bf1f6e96c98a8254bae5cfc3cf644a9d8d1
Ivan Sokolovskii <ivan3177@gmail.com>
2019-01-14 17:16:51 +0300

    Implement accept the answer as best answer

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Answer;
 
 class Question extends Model
 {
@@ -48,5 +49,11 @@ class Question extends Model
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function acceptBestAnswer(Answer $answer)
+    {
+        $this->best_answer_id = $answer->id;
+        $this->save();
     }
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,9 +17,20 @@
                         <a class="vote-down off" title=" This answer is not useful">
                             <i class="fas fa-caret-down fa-3x"></i>
                         </a>
-                        <a title="Click to mark as best answer" class="{{ $answer->status }} mt-2">
+                        @can ('accept', $answer)
+                        <a title="Click to mark as best answer" class="{{ $answer->status }} mt-2" onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();">
                             <i class="fas fa-check fa-2x"></i>
                         </a>
+                        <form style="display: none;" id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="post">
+                            @csrf
+                        </form>
+                        @else
+                        @if ($answer->is_best)
+                        <a title="The question owner accepted this answer as best answer" class="{{ $answer->status }} mt-2">
+                            <i class="fas fa-check fa-2x"></i>
+                        </a>
+                        @endif
+                        @endcan
                     </div>
                     <div class="media-body">
                         {!! $answer->body !!}
